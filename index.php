575a809e5488c9fbb4ae1be6ebb0ea649ee0eac2
EmilieVrd <74055277+EmilieVrd@users.noreply.github.com>
2021-01-25 15:42:15 +0100

    Création index.php et header.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Pliage31</title>
+<link href="ASSETS/BOOTSTRAP/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="ASSETS/bootstrap/js/bootstrap.bundle.min.js">
+<link href="header.css" rel="stylesheet" type="text/css">
+<link rel="icon" href="favicon.ico" />
+</head>
+
+<body>
+<header>
+	<?php require 'header.php' ;?>
+</header>
+</body>
+</html>
