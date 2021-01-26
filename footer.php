5443b93651553b0fd1675185e4e84ea6555893ed
EmilieVrd <74055277+EmilieVrd@users.noreply.github.com>
2021-01-26 15:27:34 +0100

    Ajout footer.php/footer.css + cards bootstrap

---
+++ footer.php
@@ -0,0 +1,7 @@
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-4">PLACEHOLDER</div>
+			<div class="col-lg-4">PLACEHOLDER</div>
+			<div class="col-lg-4">PLACEHOLDER</div>
+		</div>
+	</div>
--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 	<title>Pliage31</title>
 	<link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
 	<link href="assets/css/header.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/style.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/footer.css" rel="stylesheet" type="text/css">
 	<link rel="icon" href="favicon.ico" />
 </head>
 
@@ -52,6 +54,87 @@
   </a>
 </div>
 <!--________________________________________ /SLIDESHOW ___________________________________________-->
+	
+<!--________________________________________ /CARDS ___________________________________________-->
+<section id=""> 
+  <div class="container"> 
+    <!--___1st row___-->
+    <div class="row no-gutters">
+      <div class="col-lg-4">
+        <div class="news-card"> <a href="#" class="news-card__card-link"></a> <img src="https://www.pliage31.com/s/cc_images/cache_68027363.jpg?t=1552301316" alt="photo couvertine alu" class="news-card__image">
+          <div class="news-card__text-wrapper">
+            <h2 class="news-card__title">Couvertines et Anti-goutte</h2>
+            <div class="news-card__details-wrapper">
+              <p class="news-card__excerpt">
+              <ul>
+                <li>Anti-gouttes / Solins</li>
+                <li>Couvertines droites et arrondies</li>
+                <li>Appuis de fenetres</li>
+                <li>Entourages de fenetre pour ITE</li>
+                </br>
+                Fabrications sur mesures
+              </ul>
+              </p>
+              <a href="#" class="news-card__read-more">En voir plus <i class="fas fa-long-arrow-alt-right"></i></a> </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="news-card"> <a href="#" class="news-card__card-link"></a> <img src="https://www.pliage31.com/s/cc_images/cache_68133538.jpg?t=1553004923" alt="photo finitions portes et fenêtres" class="news-card__image">
+          <div class="news-card__text-wrapper">
+            <h2 class="news-card__title">Finitions portes et fenêtres</h2>
+            <div class="news-card__details-wrapper">
+              <p class="news-card__excerpt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est pariatur nemo tempore repellat? Ullam sed officia iure architecto deserunt distinctio&hellip;</p>
+              <a href="#" class="news-card__read-more">Read more <i class="fas fa-long-arrow-alt-right"></i></a> </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="news-card"> <a href="#" class="news-card__card-link"></a> <img src="https://www.pliage31.com/s/cc_images/cache_68036176.jpg?t=1552314854" alt="" class="news-card__image">
+          <div class="news-card__text-wrapper">
+            <h2 class="news-card__title">Habillages de façades</h2>
+            <div class="news-card__details-wrapper">
+              <p class="news-card__excerpt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est pariatur nemo tempore repellat? Ullam sed officia iure architecto deserunt distinctio&hellip;</p>
+              <a href="#" class="news-card__read-more">Read more <i class="fas fa-long-arrow-alt-right"></i></a> </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--___2nd row___-->
+    <div class="row no-gutters">
+      <div class="col-2"></div>
+      <div class="col-lg-4">
+        <div class="news-card"> <a href="#" class="news-card__card-link"></a> <img src="https://www.pliage31.com/s/cc_images/cache_68036186.jpg?t=1552315686" alt="" class="news-card__image">
+          <div class="news-card__text-wrapper">
+            <h2 class="news-card__title">Faux-plafonds</h2>
+            <div class="news-card__details-wrapper">
+              <p class="news-card__excerpt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est pariatur nemo tempore repellat? Ullam sed officia iure architecto deserunt distinctio&hellip;</p>
+              <a href="#" class="news-card__read-more">Read more <i class="fas fa-long-arrow-alt-right"></i></a> </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="news-card"> <a href="#" class="news-card__card-link"></a> <img src="https://www.pliage31.com/s/cc_images/cache_68133586.jpg?t=1553005233" alt="" class="news-card__image">
+          <div class="news-card__text-wrapper">
+            <h2 class="news-card__title">Inox pour les cuisines et autres</h2>
+            <div class="news-card__details-wrapper">
+              <p class="news-card__excerpt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est pariatur nemo tempore repellat? Ullam sed officia iure architecto deserunt distinctio&hellip;</p>
+              <a href="#" class="news-card__read-more">Read more <i class="fas fa-long-arrow-alt-right"></i></a> </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-2"></div>
+    </div>
+  </div>
+  </div>
+</section>
+<!--________________________________________ /CARDS ___________________________________________-->
+	
+<!--________________________________________ /FOOTER ___________________________________________-->
+<footer>
+  <?php require 'footer.php' ;?>
+</footer>
+<!--________________________________________ /FOOTER ___________________________________________-->
 
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
