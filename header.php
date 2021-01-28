ea8c5a50dd4585ababa0bf0774fa0ca41936d8ae
Maxou <73638394+MaxouDEV@users.noreply.github.com>
2021-01-28 16:25:35 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
  	<nav class="navbar navbar-expand-lg bg-light">
 
-    <a class="navbar-brand" href="#"><img src="assets/images/logo/logo.png" alt="logo"></a>
+    <a class="navbar-brand" href="#"><img class="logo" src="assets/images/logo/logo.png" alt="logo"></a>
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
