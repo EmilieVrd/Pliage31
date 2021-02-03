e76fdd7851ff799c707eae53ff464ca66112c2ab
EmilieVrd <74055277+EmilieVrd@users.noreply.github.com>
2021-02-03 14:43:43 +0100

    Changement dispo dessinersapièce

---
+++ assets/js/script_pages.js
@@ -0,0 +1,8 @@
+$("#collapseOne']").click(function()
+{
+    $('#exampleRadios2').prop('checked',true);
+});
+ $("a[href='#collapse2']").click(function()
+{
+    $('#optionsRadios1').prop('checked',true);
+});
--- index.php
+++ index.php
@@ -209,11 +209,7 @@
 31140 Aucamville
 Téléphone : 05 62 10 26 22
 Fax :05.62.10.54.20
-Email : contact@pliage31.com
-</p>
-© Pliage31 | <a href="pages/mentionslegales.php">Mentions légales</a>
-<div class="accespages">
-  <a href="pages/galerie_couvertine.php">Couvertines et gouttes</a> | <a href="pages/galerie_finitions.php">Finitions portes et fenêtres</a> | <a href="pages/galerie_façade.php">Façades</a> | <a href="pages/galerie_faux_plafonds.php">Faux Plafonds</a> | <a href="pages/galerie_inox_cuisine_autre.php">Inox cuisines et autres</a>
+Email : contact@pliage31.com</p>
 		</div>
 	</div>
 	</div>
