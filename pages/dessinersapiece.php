c8b711e9849df8d6337143d35b17f5942d6c0ee3
keithvald <73640852+keithvald@users.noreply.github.com>
2021-02-03 10:10:26 +0100

    ajout dessiner piece

--- pages/dessinersapiece.php
+++ pages/dessinersapiece.php
@@ -5,28 +5,274 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Dessiner sa pièce</title>
+	<title>Dessiner votre pièce</title>
 	<link href="../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-	<link href="../assets/css/style_pages.css" rel="stylesheet" type="text/css">
+	<link href="../assets/css/style_dsp.css" rel="stylesheet" type="text/css">
 	<link href="../assets/css/header.css" rel="stylesheet" type="text/css">
 	<link href="../assets/css/footer.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
+	<!--________________________________________ HEADER ___________________________________________-->
 	<header>
 		<?php require 'header.php' ;?>
-	</header>
-	
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-8">
-				
-			</div>
-			<div class="col-lg-4">
-				
+	</header>	
+	<!--________________________________________ /HEADER___________________________________________-->
+
+
+	<!--________________________________________TITRE ___________________________________________-->
+	<div class="text-center">
+		<h1>Dessinez votre pièce sur mesure !</h1>
+	</div>
+	<!--_______________________________________/TITRE ___________________________________________-->
+
+
+	<!--________________________________________ PAGE ___________________________________________-->
+
+<div class="container-fluid my-4">
+	<div class="row">
+		<div class="col-lg-6">
+					<!--------------------------------------CARD------------------------------------->
+
+
+
+<div class="container my-3 first">
+	<div class="metal py-1 text-center bg-light"><h2>Choisissez Votre Tôle</h2></div>
+   				 <div class="container my-4">
+   				 	<div class="row">
+				    <!--.second slide-->
+				      <div class="col-md-4" >
+				       <div class="card mb-auto">
+				          <img class="card-img-top"
+				            src="../assets/images/dessiner_piece/toles/alu_damier.jpg" alt="Card image cap">
+				          <div class="card-body">
+				            <h2 class="card-title">Card title</h2>
+				            
+				            <a class="btn btn-primary">Button</a>
+				          </div>
+				        </div>
+				      </div>
+
+				      <div class="col-md-4" >
+				        <div class="card mb-auto">
+				          <img class="card-img-top"
+				            src="../assets/images/dessiner_piece/toles/alu_damier.jpg" alt="Card image cap">
+				          <div class="card-body">
+				            <h2 class="card-title">Card title</h2>
+				            
+				            <a class="btn btn-primary">Button</a>
+				          </div>
+				        </div>
+				      </div>
+
+				      <div class="col-md-4" >
+				        <div class="card mb-auto">
+				          <img class="card-img-top"
+				            src="../assets/images/dessiner_piece/toles/alu_damier.jpg" alt="Card image cap">
+				          <div class="card-body">
+				            <h2 class="card-title">Card title</h2>
+				            
+				            <a class="btn btn-primary">Button</a>
+				          </div>
+				        </div>
+				      </div>
+				      
+					</div>
+					<!--/row-->
+				</div>
+				<!--/.container-->
+<hr/>
+   				 <div class="container my-4">
+   				 	<div class="row">
+				    <!--.second slide-->
+				      <div class="col-md-4" >
+				       <div class="card mb-auto">
+				          <img class="card-img-top"
+				            src="../assets/images/dessiner_piece/toles/alu_damier.jpg" alt="Card image cap">
+				          <div class="card-body">
+				            <h2 class="card-title">Card title</h2>
+				            
+				            <a class="btn btn-primary">Button</a>
+				          </div>
+				        </div>
+				      </div>
+
+				      <div class="col-md-4" >
+				        <div class="card mb-auto">
+				          <img class="card-img-top"
+				            src="../assets/images/dessiner_piece/toles/alu_damier.jpg" alt="Card image cap">
+				          <div class="card-body">
+				            <h2 class="card-title">Card title</h2>
+				            
+				            <a class="btn btn-primary">Button</a>
+				          </div>
+				        </div>
+				      </div>
+
+				      <div class="col-md-4" >
+				        <div class="card mb-auto">
+				          <img class="card-img-top"
+				            src="../assets/images/dessiner_piece/toles/alu_damier.jpg" alt="Card image cap">
+				          <div class="card-body">
+				            <h2 class="card-title">Card title</h2>
+				            
+				            <a class="btn btn-primary">Button</a>
+				          </div>
+				        </div>
+				      </div>
+				      
+					</div>
+					<!--/row-->
+				</div>
+				<!--/.container-->
+<hr/>
+  				 <div class="container my-4">
+   				 	<div class="row">
+				    <!--.second slide-->
+				      <div class="col-md-4" >
+				       <div class="card mb-auto">
+				          <img class="card-img-top"
+				            src="../assets/images/dessiner_piece/toles/alu_damier.jpg" alt="Card image cap">
+				          <div class="card-body">
+				            <h2 class="card-title">Card title</h2>
+				            
+				            <a class="btn btn-primary">Button</a>
+				          </div>
+				        </div>
+				      </div>
+
+				      <div class="col-md-4" >
+				        <div class="card mb-auto">
+				          <img class="card-img-top"
+				            src="../assets/images/dessiner_piece/toles/alu_damier.jpg" alt="Card image cap">
+				          <div class="card-body">
+				            <h2 class="card-title">Card title</h2>
+				            
+				            <a class="btn btn-primary">Button</a>
+				          </div>
+				        </div>
+				      </div>
+
+				      <div class="col-md-4" >
+				        <div class="card mb-auto">
+				          <img class="card-img-top"
+				            src="../assets/images/dessiner_piece/toles/alu_damier.jpg" alt="Card image cap">
+				          <div class="card-body">
+				            <h2 class="card-title">Card title</h2>
+				            
+				            <a class="btn btn-primary">Button</a>
+				          </div>
+				        </div>
+				      </div>
+
+					</div>
+					<!--/row-->
+				</div>
+				<!--/.container-->
+</div>
+<!--/.container-->
+
+
+		</div>
+		<!--/Col-->
+					<!--------------------------------------ACCORDEON------------------------------------->
+
+		<div class="col-lg-6">
+
+
+		<div class="container my-4">
+			<div id="accordion">
+			  <div class="card">
+			    <div class="card-header" id="headingOne">
+			      <h3 class="mb-0">
+			        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+			          TEXTURES
+			        </button>
+			      </h3>
+			    </div>
+
+			    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+			      <div class="card-body">
+			        
+			      	<p>Veuillez choisir une texture :</p>
+
+					<ul>
+					  <li>
+					    <input type="checkbox" id="myCheckbox1" />
+					    <label for="myCheckbox1"><img src="../assets/images/dessiner_piece/textures/sat.png" /></label>
+					  </li>
+
+					  <li>
+					    <input type="checkbox" id="myCheckbox2" />
+					    <label for="myCheckbox2"><img src="../assets/images/dessiner_piece/textures/mat.png" /></label>
+					  </li>
+
+					  <li>
+					    <input type="checkbox" id="myCheckbox3" />
+					    <label for="myCheckbox3"><img src="../assets/images/dessiner_piece/textures/piedra.png" /></label>
+					  </li>
+					</ul>
+
+			      </div>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <div class="card-header" id="headingTwo">
+			      <h3 class="mb-0">
+			        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+			          MESURES
+			        </button>
+			      </h3>
+			    </div>
+			    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+			      <div class="card-body">
+			        
+			      	<p>Custom Radio Buttons</p>
+
+			      	<div></div>
+					<label class="container">ep 1.5 mm, format 1500x3000
+					  <input type="radio" checked="checked" name="radio">
+					  <span class="checkmark"></span>
+					</label>
+					<label class="container">ep 1 mm, format 1500x3000
+					  <input type="radio" name="radio">
+					  <span class="checkmark"></span>
+					</label>
+					<label class="container">ep 1.5 mm,format 1500x3000 et 1500x4000
+					  <input type="radio" name="radio">
+					  <span class="checkmark"></span>
+					</label>
+					<label class="container"> ep 1 mm, format 1500x3000
+					  <input type="radio" name="radio">
+					  <span class="checkmark"></span>
+					</label>
+
+			      </div>
+			    </div>
+			  </div>
 			</div>
 		</div>
+		<!--container-->
+
+
+		</div>
+		<!--/Col-->
 	</div>
+	<!--/Row-->
+</div>
+<!--/Container-Fluid-->
+
+
+	<!--________________________________________ /PAGE ___________________________________________-->
+
+
+
+
+
+
+
+
+	<!--________________________________________ FOOTER ___________________________________________-->
 	
 	<footer>
 		<?php require 'footer.php' ;?>
