e060d60f9dc921b41dc74da074b702d5cc2cb541
EmilieVrd <74055277+EmilieVrd@users.noreply.github.com>
2021-02-04 16:41:23 +0100

    +alt +comments +favicon
    =changement de logo

--- assets/js/script_pages.js
+++
@@ -1,8 +0,0 @@
-$("#collapseOne']").click(function()
-{
-    $('#exampleRadios2').prop('checked',true);
-});
- $("a[href='#collapse2']").click(function()
-{
-    $('#optionsRadios1').prop('checked',true);
-});
--- pages/dessinersapiece.php
+++ pages/dessinersapiece.php
@@ -1,4 +1,3 @@
-​
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -6,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Dessiner votre pièce</title>
+	<title>Pièce sur mesure</title>
 	<link href="../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
 	<link href="../assets/css/style_dsp.css" rel="stylesheet" type="text/css">
 	<link href="../assets/css/header.css" rel="stylesheet" type="text/css">
@@ -21,197 +20,202 @@
 	<!--________________________________________ /HEADER___________________________________________-->
 	<!--________________________________________TITRE ___________________________________________-->
 	<div class="text-center">
-		<h1>Dessinez votre pièce sur mesure !</h1>
+		<h1>Commandez votre pièce sur mesure !</h1>
 	</div>
 	<!--_______________________________________/TITRE ___________________________________________-->
 	<!--________________________________________ PAGE ___________________________________________-->
-	<div class="container my-4">
+	<div class="container my-4 col-left ctn-first">
 		<div class="row">
-			<div class="col-lg-6 col-left">
+			<div class="col-lg-12">
 				<!--------------------------------------CARD------------------------------------->
 				<div class="metal text-center bg-light rounded">
-					<h2>Choisissez Votre Matériau</h2>
+					<h2>Nos matériaux</h2>
 				</div>
-				<div class="row">
-					<!--.second slide-->
-					<div class="card-group">
-						<div class="card ">
-							<img class="card-img-top" src="../assets/images/dessiner_piece/toles/tolesperf.png" alt="Card image cap">
-							<h3 class="card-title my-auto text-center">Tôles perforées</h3>
-						</div>
-						<a class="card stretched-link" data-toggle="collapse" data-target="#tôlegalva">
-							<img class="card-img-top" src="../assets/images/dessiner_piece/toles/tolesgalva.png" alt="Card image cap">
-							<h3 class="card-title my-auto text-center">Tôles Galvanisées</h3>
-						</a>
-						<a class="card stretched-link" data-toggle="collapse" data-target="#tôleelectro">
-							<img class="card-img-top" src="../assets/images/dessiner_piece/toles/toleselectro.png" alt="Card image cap">
-							<h3 class="card-title my-auto text-center">Tôles électro-zinguées</h3>
-						</a>
+			</div>
+				<div class="row no-gutters">
+				<div class="col-md-4 my-auto">
+				<div class="card-group">
+					<div class="card ">
+						<img class="card-img-top" src="../assets/images/dessiner_piece/toles/tolesperf.png" alt="Card image cap">
+						<h3 class="card-title my-auto text-center">Tôles perforées</h3>
 					</div>
-				</div>
-				<!--/row-->
-				<!--/.container-->
-				<hr/>
-				<div class="row">
-					<!--.second slide-->
-					<div class="card-group">
-						<div class="card">
-							<img class="card-img-top" src="../assets/images/dessiner_piece/toles/tolesDKP.png" alt="Card image cap">
-							<h3 class="card-title my-auto text-center">Tôles en acier DKP</h3>
-						</div>
-						<div class="card">
-							<img class="card-img-top" src="../assets/images/dessiner_piece/toles/tolesacierlamée.png" alt="Card image cap">
-							<h3 class="card-title my-auto text-center">Tôles en aciers larmées</h3>
-						</div>
-						<div class="card">
-							<img class="card-img-top" src="../assets/images/dessiner_piece/alu/aluannodisé.png" alt="Card image cap">
-							<h3 class="card-title my-auto text-center">Aluminium Anodisé</h3>
-						</div>
+					<a class="card stretched-link text-decoration-none" data-toggle="collapse" data-target="#tôlegalva">
+						<img class="card-img-top" src="../assets/images/dessiner_piece/toles/tolesgalva.png" alt="Card image cap">
+						<h3 class="card-title my-auto text-center">Tôles Galvanisées</h3>
+					</a>
+					<a class="card stretched-link text-decoration-none" data-toggle="collapse" data-target="#tôleelectro">
+						<img class="card-img-top" src="../assets/images/dessiner_piece/toles/toleselectro.png" alt="Card image cap">
+						<h3 class="card-title my-auto text-center">Tôles électro-zinguées</h3>
+					</a>
 					</div>
-					<!--card group-->
-				</div>
-				<!--/row-->
-				<!--/.container-->
-				<hr/>
-				<div class="row">
-					<!--.second slide-->
-					<div class="card-group">
-						<div class="card">
-							<img class="card-img-top" src="../assets/images/dessiner_piece/alu/alubrut.png" alt="Card image cap">
-							<h3 class="card-title my-auto text-center">Aluminium Brut</h3>
-						</div>
-						<div class="card">
-							<img class="card-img-top" src="../assets/images/dessiner_piece/alu/aludamiers.png" alt="Card image cap">
-							<h3 class="card-title my-auto text-center">Aluminium Damiers</h3>
-						</div>
-						<div class="card">
-							<img class="card-img-top" src="../assets/images/dessiner_piece/alu/alulaquées.png" alt="Card image cap">
-							<h3 class="card-title my-auto text-center">Tôles aluminium Laquées RAL</h3>
-						</div>
+					</div>
+								<div class="col-md-4  my-auto">
+				<div class="card-group">
+					<a class="card stretched-link text-decoration-none" data-toggle="collapse" data-target="#epDKP">
+						<img class="card-img-top" src="../assets/images/dessiner_piece/toles/tolesDKP.png" alt="Card image cap">
+						<h3 class="card-title my-auto text-center">Tôles en acier DKP</h3>
+					</a>
+					<a class="card stretched-link text-decoration-none" data-toggle="collapse" data-target="#epTôleLarmée">
+						<img class="card-img-top" src="../assets/images/dessiner_piece/toles/tolesacierlamée.png" alt="Card image cap">
+						<h3 class="card-title my-auto text-center">Tôles en aciers larmées</h3>
+					</a>
+					<a class="card stretched-link text-decoration-none" data-toggle="collapse" data-target="#epTôleAnod">
+						<img class="card-img-top" src="../assets/images/dessiner_piece/alu/aluannodisé.png" alt="Card image cap">
+						<h3 class="card-title my-auto text-center">Aluminium Anodisé</h3>
+					</a>
+									</div>
+									</div>
+								<div class="col-md-4 my-auto">
+				<div class="card-group">
+					<div class="card">
+						<img class="card-img-top" src="../assets/images/dessiner_piece/alu/alubrut.png" alt="Card image cap">
+						<h3 class="card-title my-auto text-center">Aluminium Brut</h3>
+					</div>
+					<div class="card">
+						<img class="card-img-top" src="../assets/images/dessiner_piece/alu/aludamiers.png" alt="Card image cap">
+						<h3 class="card-title my-auto text-center">Aluminium Damiers</h3>
+					</div>
+					<div class="card">
+						<img class="card-img-top" src="../assets/images/dessiner_piece/alu/alulaquées.png" alt="Card image cap">
+						<h3 class="card-title my-auto text-center">Tôles aluminium Laquées RAL</h3>
+					</div>
 					</div>
 					<!--card group-->
 				</div>
-				<!--/row-->
-				<!--/.container-->
-				<!--/.container-->
+				</div>
+			<!--/Col-->
+		</div>
+		<!--/row-->
+	</div>
+
+	<!--/Container-->
+	<div class="container col-left my-4 ctn-second">
+		<div class="metal text-center bg-light rounded">
+			<h2>Nos options</h2>
+		</div>
+		<div class="row">
+			<!--------------------------------------CARD------------------------------------->
+			<div class="col-lg-4">
+				<h3 class="text-center text-white">Finitions</h3>
+				<div class="card-group">
+					<div class="card ">
+						<img class="card-img-top" src="../assets/images/dessiner_piece/textures/mat.png" alt="Card image cap">
+						<h3 class="card-title my-auto text-center">MAT</h3>
+					</div>
+					<div class="card  text-decoration-none">
+						<img class="card-img-top" src="../assets/images/dessiner_piece/textures/piedra.png" alt="Card image cap">
+						<h3 class="card-title my-auto text-center">Piedra</h3>
+					</div>
+					<div class="card  text-decoration-none">
+						<img class="card-img-top" src="../assets/images/dessiner_piece/textures/sat.png" alt="Card image cap">
+						<h3 class="card-title my-auto text-center">SAT</h3>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 text-white">
+				<h3 class="text-center text-white">Epaisseur</h3>
+				<div class="collapse show" id="épaisseur">
+					<div class="collapse" id="epTôleAnod" data-parent="#épaisseur">
+						<label class="radio radio-gradient"> <span class="radio__input">
+						    <input type="radio" name="radio">
+						    <span class="radio__control"></span>
+							</span> <span class="radio__label">1mm</span>
+						</label>
+						<label class="radio radio-gradient"> <span class="radio__input">
+						    <input type="radio" name="radio" data-toggle="collapse" data-target="#dimDéfaut">
+						    <span class="radio__control"></span>
+							</span> <span class="radio__label">1,5mm</span>
+						</label>
+						<label class="radio radio-gradient"> <span class="radio__input">
+						    <input type="radio" name="radio" data-toggle="collapse" data-target="#dimDéfaut">
+						    <span class="radio__control"></span>
+							</span> <span class="radio__label">2mm</span>
+						</label>
+					</div>
+					<div class="collapse" id="epDKP" data-parent="#épaisseur">
+						<label class="radio radio-gradient"> <span class="radio__input">
+						    <input type="radio" name="radio" data-toggle="collapse" data-target="#dimDéfaut">
+						    <span class="radio__control"></span>
+							</span> <span class="radio__label">1,5mm</span>
+						</label>
+						<label class="radio radio-gradient"> <span class="radio__input">
+						    <input type="radio" name="radio" data-toggle="collapse" data-target="#dimDéfaut">
+						    <span class="radio__control"></span>
+							</span> <span class="radio__label">2mm</span>
+						</label>
+					</div>
+					<div class="collapse" id="tôlegalva" data-parent="#épaisseur">
+						<label class="radio radio-gradient"> <span class="radio__input">
+						    <input type="radio" name="radio" data-toggle="collapse" data-target="#dimDéfaut">
+						    <span class="radio__control"></span>
+							</span> <span class="radio__label">0.8mm</span>
+						</label>
+						<label class="radio radio-gradient"> <span class="radio__input">
+						    <input type="radio" name="radio" data-toggle="collapse" data-target="#dimDéfaut">
+						    <span class="radio__control"></span>
+							</span> <span class="radio__label">1mm</span>
+						</label>
+						<label class="radio radio-gradient"> <span class="radio__input">
+						    <input type="radio" name="radio">
+						    <span class="radio__control"></span>
+							</span> <span class="radio__label">1,5 mm</span>
+						</label>
+						<label class="radio radio-gradient"> <span class="radio__input">
+						    <input type="radio" name="radio">
+						    <span class="radio__control"></span>
+							</span> <span class="radio__label">2mm</span>
+						</label>
+						<label class="radio radio-gradient"> <span class="radio__input">
+						    <input type="radio" name="radio">
+						    <span class="radio__control"></span>
+							</span> <span class="radio__label">4mm</span>
+						</label>
+					</div>
+					<div class="collapse" id="tôleelectro" data-parent="#épaisseur">
+						<label class="radio radio-gradient"> <span class="radio__input">
+						    <input type="radio" name="radio">
+						    <span class="radio__control"></span>
+							</span> <span class="radio__label">1,5 mm</span>
+						</label>
+						<label class="radio radio-gradient"> <span class="radio__input">
+						    <input type="radio" name="radio">
+						    <span class="radio__control"></span>
+							</span> <span class="radio__label">2mm</span>
+						</label>
+					</div>
+					<div class="collapse" id="epTôleLarmée" data-parent="#épaisseur">
+						<label class="radio radio-gradient"> <span class="radio__input">
+						    <input type="radio" name="radio" data-toggle="collapse" data-target="#dimDéfaut">
+						    <span class="radio__control"></span>
+							</span> <span class="radio__label">4mm</span>
+						</label>
+					</div>
+				</div>
+			</div>
+			<!--col-->
+			<div class="col-lg-4">
+				<h3 class="text-center text-white">Couleurs</h3>
+				<div class="card-group">
+					<div class="card">
+						<img class="card-img-top" src="../assets/images/dessiner_piece/alu/alubrut.png" alt="Card image cap">
+						<h3 class="card-title my-auto text-center">Aluminium Brut</h3>
+					</div>
+					<div class="card">
+						<img class="card-img-top" src="../assets/images/dessiner_piece/alu/aludamiers.png" alt="Card image cap">
+						<h3 class="card-title my-auto text-center">Aluminium Damiers</h3>
+					</div>
+					<div class="card">
+						<img class="card-img-top" src="../assets/images/dessiner_piece/alu/alulaquées.png" alt="Card image cap">
+						<h3 class="card-title my-auto text-center">Tôles aluminium Laquées RAL</h3>
+					</div>
+				</div>
+				<!--card group-->
 			</div>
 			<!--/Col-->
-			<div class="col-lg-6 twin two my-1">
-				<div class="title_container py-3 text-center bg-light">
-					<h2>Je sais pas encore</h2>
-				</div>
-				<!--TEXTURE-->
-				<button class="btn btn-primary" data-toggle="collapse" data-target="#texture" aria-expanded="true" aria-controls="texture">TEXTURE</button>
-				<p>Veuillez choisir une texture :</p>
-				<ul class="collapse" id="texture">
-					<li>
-						<div class="form-check">
-							<input class="form-check-input" type="radio" name="flexRadioDefault" id="myCheckbox1">
-							<label class="form-check-label" for="myCheckbox1">
-								<img src="../assets/images/dessiner_piece/textures/sat.png" />
-							</label>
-						</div>
-					</li>
-					<li>
-						<div class="form-check">
-							<input class="form-check-input" type="radio" name="flexRadioDefault" id="myCheckbox2">
-							<label class="form-check-label" for="myCheckbox2">
-								<img src="../assets/images/dessiner_piece/textures/mat.png" />
-							</label>
-						</div>
-					</li>
-					<li>
-						<div class="form-check">
-							<input class="form-check-input" type="radio" name="flexRadioDefault" id="myCheckbox3">
-							<label class="form-check-label" for="myCheckbox3">
-								<img src="../assets/images/dessiner_piece/textures/piedra.png" />
-							</label>
-						</div>
-					</li>
-				</ul>
-				<hr/>
-				<!--EPAISSEUR-->
-				<button class="btn btn-primary show" aria-expanded="false" aria-controls="épaisseur">EPAISSEUR</button>
-				<div class="collapse show" id="épaisseur">
-					<div class="collapse" id="tôlegalva">
-					<label class="radio radio-gradient"> <span class="radio__input">
-						    <input type="radio" name="radio">
-						    <span class="radio__control"></span>
-						</span> <span class="radio__label">0.8mm</span>
-					</label>
-					<label class="radio radio-gradient"> <span class="radio__input">
-						    <input type="radio" name="radio">
-						    <span class="radio__control"></span>
-						</span> <span class="radio__label">1mm</span>
-					</label>
-					<label class="radio radio-gradient"> <span class="radio__input">
-						    <input type="radio" name="radio">
-						    <span class="radio__control"></span>
-						</span> <span class="radio__label">1,5 mm</span>
-					</label>
-					<label class="radio radio-gradient"> <span class="radio__input">
-						    <input type="radio" name="radio">
-						    <span class="radio__control"></span>
-						</span> <span class="radio__label">2mm</span>
-					</label>
-					<label class="radio radio-gradient"> <span class="radio__input">
-						    <input type="radio" name="radio">
-						    <span class="radio__control"></span>
-						</span> <span class="radio__label">4mm</span>
-					</label>
-					</div>
-					<div class="collapse" id="tôleelectro">
-					<label class="radio radio-gradient"> <span class="radio__input">
-						    <input type="radio" name="radio">
-						    <span class="radio__control"></span>
-						</span> <span class="radio__label">1,5 mm</span>
-					</label>
-					<label class="radio radio-gradient"> <span class="radio__input">
-						    <input type="radio" name="radio">
-						    <span class="radio__control"></span>
-						</span> <span class="radio__label">2mm</span>
-					</label>
-					</div>
-				</div>
-				<hr/>
-				<!--FORMAT-->
-				<button class="btn btn-primary
- collapsed" data-toggle="collapse" data-target="#format" aria-expanded="false" aria-controls="format">FORMAT</button>
-				<div class="radio2 collapse" id="format">
-					<label class="radio radio-gradient"> <span class="radio__input">
-						    <input type="radio" name="radio1">
-						    <span class="radio__control"></span>
-						</span> <span class="radio__label">format</span>
-					</label>
-					<label class="radio radio-gradient"> <span class="radio__input">
-						    <input type="radio" name="radio1">
-						    <span class="radio__control"></span>
-						</span> <span class="radio__label">format</span>
-					</label>
-					<label class="radio radio-gradient"> <span class="radio__input">
-						    <input type="radio" name="radio1">
-						    <span class="radio__control"></span>
-						</span> <span class="radio__label">format</span>
-					</label>
-					<label class="radio radio-gradient"> <span class="radio__input">
-						    <input type="radio" name="radio1">
-						    <span class="radio__control"></span>
-						</span> <span class="radio__label">format</span>
-					</label>
-					<label class="radio radio-gradient"> <span class="radio__input">
-						    <input type="radio" name="radio">
-						    <span class="radio__control"></span>
-						</span> <span class="radio__label">format</span>
-					</label>
-				</div>
-			</div>
-			<!--container-->
 		</div>
-		<!--/Col-->
+		<!--/row-->
 	</div>
-	<!--/Row-->
-	</div>
-	<!--/Container-Fluid-->
+	<!--/Container-->
 	<!--________________________________________ /PAGE ___________________________________________-->
 	<!--________________________________________ FOOTER ___________________________________________-->
 	<footer>
